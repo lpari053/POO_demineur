ece6f1406b759871812c195bc70d278dbda1d02b
lpari053 <135244212+lpari053@users.noreply.github.com>
2023-11-14 17:06:13 +0100

    update final

--- programme_avec_interface_graphique/Grille.py
+++ programme_avec_interface_graphique/Grille.py
@@ -1,12 +1,31 @@
+"""""
+CLASSE GRILLE
+
+Cette classe defini notre grille de jeu et ses actualisation et sa generation
+
+Cette classe est utiliser par la joueur_interface
+
+Attribut :
+
+        + difficule
+        + nombre_ligne
+        +nombre_colonne
+        + combien_bombe
+        + matrice
+
+"""""
+
 import numpy as np
 import random
 import sys
 
-sys.setrecursionlimit(2000)
 class Grille:
  
     def __init__(self,difficulte):
         
+
+        #Pour definir la grille il nous faut la difficulté qui permet de defnir ensuite les autres attributs
+
         self.difficulte=difficulte
         
         if (self.difficulte==1):
@@ -28,32 +47,33 @@ class Grille:
             self.nombre_colonne=20
             self.combien_bombe=20
 
-
+        #la matrice est de taille selon la diffuclte
         self.matrice=np.zeros((self.nombre_ligne,self.nombre_colonne),dtype=object)
         for i in range(self.matrice.shape[0]):
             for j in range (self.matrice.shape[1]):
+                #Pour l'instant aucune case de depart selectionner donc initialiser à zero
                 self.matrice[i,j]='O'
 
-        
+    #Avec l'information de case de depart nous pouvons genrer la matrice avec les bombes    
     def generation_matrice(self,case_dep):
         
         liste_bombe=[]
-        print(liste_bombe)
         
-        for i in range(self.combien_bombe):
-            
+
+        #place de maniere aleatoire dans la matrice les bombes sans qu'il y est des bombes sur case de depart
+        while len(liste_bombe)!=self.nombre_ligne:
             casie=[random.randint(0, self.nombre_ligne-1),random.randint(0, self.nombre_ligne-1)]
             
             if casie!=case_dep:
-            
-                liste_bombe.append(casie)
-            
-            else:
-                i=i-1
-            
+
+                if casie not in liste_bombe:
+                    liste_bombe.append(casie)
+                    
+
+        #om met la position des bombes dans une liste    
         self.liste_bombe=liste_bombe
         
-        
+    #fonction pour savoir le nombre autoir de bombe pour une case
     def nombre_bombe_autour(self,case):
         
         grilliade=self
@@ -62,37 +82,23 @@ class Grille:
             for j in [-1,0,1]:
                 if [case[0]+i,case[1]+j] in grilliade.liste_bombe:
                         count+=1
-                
+        
         return count
          
-        
+    #fonction d'aide developpeur pour afficher la matrice du jeu
     def afficher_matrice(self):
         
         custom_format = np.array2string(self.matrice, separator=', ', formatter={'all': lambda x: str(x)})
 
         print(custom_format)
         
-        
-    def devoiler_matrice(self):
-        
-        
-        rtyu=self.matrice
-        
-        for case_bombe in self.liste_bombe:
-            
-            x,y=case_bombe
-            
-            self.matrice[x,y]="B"
-        
-        self.afficher_matrice()
-        
-        self.matrice=rtyu
+
         
 
         
-        
+    # fonction qui fait les zones avec les nombres autour d'une case
     def faire_zone(self,case):
-        
+
         liste_bombe=self.liste_bombe
         self.matrice[case[0],case[1]]=self.nombre_bombe_autour(case)
         liste_zero=[]
@@ -104,9 +110,8 @@ class Grille:
                     if(self.nombre_bombe_autour([case[0]+i,case[1]+j])==0):
                         c+=1
                         C.append([case[0]+i,case[1]+j])
-                        print('c ',c)
 
-                        self.matrice[case[0]+i,case[1]+j]='X'
+                        self.matrice[case[0]+i,case[1]+j]=self.nombre_bombe_autour([case[0]+i,case[1]+j])
                         liste_zero.append([case[0]+i,case[1]+j])
                     else:
                         if i!=j:
@@ -116,28 +121,54 @@ class Grille:
             dd=np.random.choice(C)
             self.faire_zone(dd)
 
+    #actualise la matrice quand action faite pour afficher nombre de facon aleatoire
+    def nouv_zone(self,case):
 
+        liste_bombe=self.liste_bombe
+        self.matrice[case[0],case[1]]=self.nombre_bombe_autour(case)
 
-        
+        i=np.random.choice([-1,0,1])
+        j=np.random.choice([-1,0,1])
 
-                            
-                          
+        if i!=0 and j!=0:
+
+            if [case[0]+i,case[1]+j] not in liste_bombe:
+
+                
+
+                try:
+
+                    self.matrice[case[0]+i,case[1]+j]=self.nombre_bombe_autour([case[0]+i,case[1]+j])
+
+                except:
+
+                    self.nouv_zone(case)
+
             
 
-        
+            else:
+
+                self.nouv_zone(case)
+        else:
+            self.nouv_zone(case)
+
+
+
+ 
+    #savoir si le jeu est gagane ou perdu selon les drapeau placer
     def fini_ou_non(self):
         grilliade=self
-        plus_de_zero=np.isin(grilliade.matrice,0)
         a=grilliade.matrice
         ou_drapeau=np.where(a=='D')
         liste_drapeau= [list(x) for x in zip(ou_drapeau[0], ou_drapeau[1])]
-        
-        if liste_drapeau==grilliade.liste_bombe:
+        k=0
+        for l in liste_drapeau:
+            if l in grilliade.liste_bombe:
+                k+=1
+            if k==len(grilliade.liste_bombe):
+                print("Vous avez gagne")
             
-            print("Vous avez gagne")
-            
-            return True
-                 
+                return True
         else:
             
             return False
--- programme_avec_interface_graphique/joueur_interface.py
+++ programme_avec_interface_graphique/joueur_interface.py
@@ -1,78 +1,91 @@
+"""""
+CLASSE JOUEUR_INTERFACE
+
+Cette classe est la classe qui defini le joueur et ses actions au sein du jeu.
+
+
+
+Attribut :
+
+        +etat_jeu
+        +grille
+
+
+"""""
+
+
 from Grille import Grille
 from Case_normale import Case_normale
 from Mine import Mine
-
-
 import numpy as np
 
-import sys
 
 class Joueur:
-    
+    #Initilaisation du joueur 
+
     def __init__(self):
         
-        self.etat_jeu="pret a jouer"
-        
-
-    def demarrage(self):
-        
-        print("Demarrer")
-        
+        self.etat_jeu=False
+                
         
     def GetEtat_jeu(self):
         return self.etat_jeu
         
     
-    
+    #Fonction qui initilise l'attribut de difficulte selon choix appelle dans interface
     def choix_difficulte(self,difficulte):
         
         if int(difficulte) in [1,2,3]:
         
             self.grille=Grille(int(difficulte))
     
-            
-        print('difficulte choisi')
         
+    #Fonction qui prend les coordonne de case depart chosi dans l'interface pour generer la grille  
         
     def choix_case_depart(self,case_dep_x,case_dep_y):
         
+
         grilliade=self.grille
         
         self.case_depart=Case_normale("decouvert",int(case_dep_x),int(case_dep_y),"Case_normale")
        
         grilliade.matrice[int(case_dep_x),int(case_dep_y)]="X"
         
-        
+        #Nusgenerons la matrice depuis la class Grille grace au coordonnes de case depart
         grilliade.generation_matrice([int(case_dep_x),int(case_dep_y)])
         
+        #Nous adaptons la matrice autoiur de la case depart avec les numeros
         grilliade.faire_zone([int(case_dep_x),int(case_dep_y)])
 
+        #Mise a jour etat de jeu 
         self.etat_jeu='en jeu'
 
-        print('case_depart_choisi')
-        
+    
+    #Fonction de mise en drapeau selon choix dans l'interface par le joueur 
     def mettre_drapeau(self,x,y):
 
         grilliade=self.grille
-        
+        #Actualisation de la matrice pour mettre drapeau ou l'enlever si deja drapeau sur la case
         if grilliade.matrice[x,y]=='D':
             
-            grilliade.matrice[x,y]=0
+            grilliade.matrice[x,y]='O'
 
-        grilliade.matrice[x,y]='D'
+        else:
 
+            grilliade.matrice[x,y]='D'
+
+        #test si ou â gagne ou non 
         if len(np.where(grilliade.matrice=='D'))==len(grilliade.liste_bombe):
             r_etat=grilliade.fini_ou_non()
-        
             if r_etat==True:
                 self.etat_jeu='gagne'
                 
             else:
                 self.etat_jeu='perdu'
 
-        print('drapeau')
+        print('Vous avez mis un drapeau ')
 
-
+    #Fonction demine selon choix dans l'interface par le joueur 
     def mettre_bombe(self,x,y):
 
         grilliade=self.grille
@@ -85,151 +98,36 @@ class Joueur:
             
             choisi.explose()
             
+            #mise a jour de la grille
             grilliade.matrice[x,y]="B"
             
-            self.etat_jeu='perdu'
+            self.etat_jeu='perdu'  
    
         else:
             
-            liste_bombe=grilliade.liste_bombe
-            count_bombe=0
-            for k in [-1,0,1]:
-                for l in [-1,0,1]:
-                    try:
-                        f=[x+k,y+l]
-                        
-                        if f in liste_bombe:
-                            count_bombe+=1
-                        
-                    except:
-                        pass
-            
-            grilliade.matrice[x,y]=count_bombe
+            grilliade.matrice[x,y]=grilliade.nombre_bombe_autour([x,y])
 
 
         if len(np.where(grilliade.matrice=='D'))==len(grilliade.liste_bombe):
             r_etat=grilliade.fini_ou_non()
-        
             if r_etat==True:
                 self.etat_jeu='gagne'
                 
             else:
                 self.etat_jeu='perdu'
+                print('les drapeau sont pose au mauvaise endroit')
 
-        print("bombe")
+
+        grilliade.nouv_zone([x,y])
+
+        print("Vous avez deminez")
       
 
-
-    def choix_click(self):
-        
-        grilliade=self.grille
-        
-        
-        difficulte=grilliade.difficulte
-        
-        
-        if difficulte==1:
-            maxou=5
             
-        if difficulte==2:
-            maxou=10
-            
-        if difficulte==3:
-            maxou=20
-        
-        print("Voulez vous mettre un drapeau( mettre 1) ou deminer (mettre 0) ou arreter (mettre 3)")
-        action=input()
-        
-        if action==3 or action=='3':
-            self.etat_jeu='abandon'
-        
-  
-        if action=='O' or action==0 or action=='0':
-            
-            print("Quelle case voulez vous deminer en x entre 1 et ",maxou)
-            
-            x=int(input())
-            
-            
-            print("Quelle case voulez vous deminer en y entre 1 et ",maxou)
-            
-            y=int(input())
-            
-            case=[x,y]
-            
-            if case in grilliade.liste_bombe:
-                
-                choisi=Mine('decouvert',x,y,'Mine')
-                
-                choisi.explose()
-                
-                grilliade.matrice[x,y]="B"
-                
-                self.etat_jeu='perdu'
-                
-                
-            else:
-                
-                liste_bombe=grilliade.liste_bombe
-                count_bombe=0
-                for k in [-1,0,1]:
-                    for l in [-1,0,1]:
-                        try:
-                            f=[x+k,y+l]
-                            
-                            if f in liste_bombe:
-                                count_bombe+=1
-                            
-                        except:
-                            pass
-                
-                grilliade.matrice[x,y]=count_bombe
-                
-                
-        elif action==1 or action=='1':
-            
-            print("Quelle case voulez vous mettre un drapeau en x entre 1 et ",maxou)
-            
-            x=int(input())
-            
-            
-            print("Quelle case voulez vous mettre un drapeau en y entre 1 et ",maxou)
-            
-            y=int(input())
-            
-            case=[x,y]
-            
-            if grilliade.matrice[x,y]=='D':
-                
-                print('Voulez vous enlevez le drapeau ? Oui (marquez 0) Non (marquez 1)')
-                
-            
-                act=input()
-                
-                if act==0 :
-                    
-                    grilliade.matrice[x,y]=0
-                    
-            
-            
-            grilliade.matrice[x,y]='D'
-       
-        if len(np.where(grilliade.matrice=='D'))==len(grilliade.liste_bombe):
-            r_etat=grilliade.fini_ou_non()
-        
-            if r_etat==True:
-                self.etat_jeu='gagne'
-                
-            else:
-                self.etat_jeu='perdu'
-            
-        
+        #Fonction qui indique si le jeu est termine ou non 
     def fini(self):
         
         if self.etat_jeu=='perdu':
-            print('vous avez perdu ahahahaha')
-            
-            self.grille.devoiler_matrice()
             return True
         
         elif self.etat_jeu=='gagne':
@@ -244,34 +142,3 @@ class Joueur:
             return False
     
     
-    # def jeu(self):
-        
-        
-    #     print('Bienvenue dans le demineur,\n Quelle est votre nom ?')
-    #     nom=input()
-    #     moi=self
-        
-    #     self.nom=nom
-    #     self.etat_jeu='debut'
-        
-    #     moi.choix_difficulte()
-        
-    #     moi.choix_case_depart()
-        
-    #     moi.etat_jeu='joue'
-        
-    #     count=0
-        
-    #     while moi.fini()==False:
-    #         moi.choix_click()
-            
-    #         if moi.fini()==True:
-    #             break
-        
-            
-        
-
-
-# moi=Joueur()
-
-# moi.jeu()
